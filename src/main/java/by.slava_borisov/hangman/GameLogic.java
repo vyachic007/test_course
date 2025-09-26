bdc0827803d86e92e711001cfdccd33f65e6499c
Vyacheslav Borisov <vyachic.007@gmail.com>
2025-09-26 22:02:38 +0300

    complete game logic

--- src/main/java/by.slava_borisov/hangman/GameLogic.java
+++ src/main/java/by.slava_borisov/hangman/GameLogic.java
@@ -1,8 +1,50 @@
 package by.slava_borisov.hangman;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class GameLogic {
+    private WordBank wordBank;
 
-    public void initGame() {
-
+    public GameState initGame() {
+        String word = wordBank.getRandomWord();
+        GameState gameState = new GameState(word);
+        return gameState;
     }
+
+    public boolean processGuess(GameState state, char letter) {
+        state.addUserLatter(letter);
+        String secretWord = state.getSecretWord();
+        StringBuilder hiddenWord = state.getHiddenWord();
+        boolean letterFound = secretWord.contains(String.valueOf(letter));
+
+        if (letterFound) {
+            for (int i = 0; i < secretWord.length(); i++) {
+                if (secretWord.charAt(i) == letter) {
+                    hiddenWord.setCharAt(i, letter);
+                }
+            }
+            return true;
+        } else {
+            state.decreaseLives();
+            return false;
+        }
+    }
+
+    public boolean isGameWon(GameState state) {
+        StringBuilder secretWord = state.getHiddenWord();
+        for (int i = 0; i < secretWord.length(); i++) {
+            if (secretWord.charAt(i) == '_') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public boolean isGameOver(GameState state) {
+        return state.getRemainingLives() <= 0 || isGameWon(state);
+    }
+
 }
+
