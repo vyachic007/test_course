5ae024f780ac14ef31cbbf13ce4f0041197d6f61
Vyacheslav Borisov <vyachic.007@gmail.com>
2025-09-27 00:32:28 +0300

    impl ExchangeRates class

---
+++ src/main/java/by.slava_borisov/currency/ExchangeRates.java
@@ -0,0 +1,35 @@
+package by.slava_borisov.currency;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+public class ExchangeRates {
+    private Map<String, Double> rates = new HashMap<>();
+    private String baseCurrency;
+
+    public void addRate(String currency, double rate) {
+        rates.put(currency, rate);
+    }
+
+    public Double getRate(String currency) {
+       return rates.get(currency);
+    }
+
+
+    public Set<String> getAvailableCurrencies() {
+        return rates.keySet();
+    }
+
+    public void initializeDefaultRates() {
+        rates.put("RUB", 1.0);
+        rates.put("USD", 83.61);
+        rates.put("EUR", 97.68);
+        rates.put("KZT", 0.15);
+        rates.put("CNY", 11.68);
+    }
+}
+
+
+
---
+++ src/main/java/by.slava_borisov/currency/Main.java
@@ -0,0 +1,7 @@
+package by.slava_borisov.currency;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
